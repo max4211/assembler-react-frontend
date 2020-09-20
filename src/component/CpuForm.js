09b21120e25ab436446dca96b739ef30bbcce541
max <max.smith@duke.edu>
2020-09-20 16:30:03 -0400

    Updated assembler endpoint to cname within same domain

--- src/component/CpuForm.js
+++ src/component/CpuForm.js
@@ -37,7 +37,8 @@ class CpuForm extends React.Component {
     handleSubmit(event) {
         console.log("attempting axios post request");
         // const myURL = "http://localhost:8080/api/v1/assemble/".concat(this.state.type, "/", this.state.base);
-        const myURL = "http://assembler350-env.eba-5sjpqp28.us-east-1.elasticbeanstalk.com//api/v1/assemble/".concat(this.state.type, "/", this.state.base);
+        // const myURL = "https://localhost:443/api/v1/assemble/".concat(this.state.type, "/", this.state.base);
+        const myURL = "http://assembler.ece350.com/api/v1/assemble/".concat(this.state.type, "/", this.state.base);
         const formData = new FormData();
         formData.append("file", this.state.file);
         axios({
