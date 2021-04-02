8c03067a07670e385b341ad3f1ecb6c82992c4a1
Max Smith <ms724@duke.edu>
2021-04-02 17:59:23 -0400

    Testing converter functionality

--- src/component/Converter.js
+++ src/component/Converter.js
@@ -14,8 +14,12 @@ export default function Converter() {
     toBase: 10,
     toValue: "",
   });
-
-  const intRegex = new RegExp("^\\d*$");
+  const [error, setError] = React.useState(false);
+  const [helperText, setHelperText] = React.useState("");
+  const regexMap = new Map();
+  regexMap.set(2, /^[01]+$/);
+  regexMap.set(10, /^\\d*$/);
+  regexMap.set(10, /^[0123456789ABCDEF]+$/);
   const options = (
     <>
       <option value={2}>Binary</option>
@@ -23,14 +27,27 @@ export default function Converter() {
       <option value={16}>Hex</option>
     </>
   );
+  const validateInput = () => {
+    const regex = regexMap.get(state.fromBase);
+    if (!regex.test(state.fromValue)) {
+      setError(true);
+      setHelperText("Please enter valid number");
+      return false;
+    } else {
+      setError(false);
+      setHelperText("");
+      return true;
+    }
+  };
   const handleSubmit = () => {
-    logger.log("handling submit");
-    const from = parseInt(state.fromValue, state.fromBase);
-    const to = from.toString(state.toBase);
-    setState({
-      ...state,
-      toValue: to,
-    });
+    if (validateInput()) {
+      const from = parseInt(state.fromValue, state.fromBase);
+      const to = from.toString(state.toBase);
+      setState({
+        ...state,
+        toValue: to,
+      });
+    }
   };
   // options, value, setValue, label
   return (
@@ -51,9 +68,12 @@ export default function Converter() {
                 type="text"
                 value={state.fromValue}
                 onChange={(e) => {
-                  if (intRegex.test(e.target.value))
+                  if (regexMap.get(10).test(e.target.value)) {
                     setState({ ...state, fromValue: e.target.value });
+                  }
                 }}
+                error={error}
+                helperText={helperText}
               />
             </div>
             <div>
